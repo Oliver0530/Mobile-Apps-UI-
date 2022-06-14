3a4d7e1513264b47cef61669dcee72ab2ae0d204
Oliver0530 <47092735+Oliver0530@users.noreply.github.com>
2022-06-14 11:52:07 +0100

    Add files via upload

---
+++ Notes/Views/Birds.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class Birds : ContentPage
+    {
+        public Birds()
+        {
+            InitializeComponent();
+
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://www.rspb.org.uk/birds-and-wildlife/wildlife-guides/bird-a-z/");
+        }
+
+        public async void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BirdPages.BluePit());
+        }
+
+        public async void TapGestureRecognizer_Tapped_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BirdPages.Page1());
+        }
+
+        public async void TapGestureRecognizer_Tapped_3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BirdPages.Page1());
+        }
+
+
+    }
+}
